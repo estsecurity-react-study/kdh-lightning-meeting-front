6e076e0022d4e768339532725061b95fe31fb1bf
김덕현 <oraclian@estsecurity.com>
2023-01-04 13:28:33 +0900

    fix: BaseTemplate 수정

--- components/templates/BaseTemplate/index.tsx
+++ components/templates/BaseTemplate/index.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import 'twin.macro';
 
 import PortalNavbar from '../../ui/organisms/PortalNavbar';
 
-const BaseTemplate = () => {
+export type BaseTemplateProps = React.PropsWithChildren;
+
+const BaseTemplate = ({ children }: BaseTemplateProps) => {
   return (
     <>
-      <main tw="min-h-screen"></main>
+      <main tw="min-h-screen">{children}</main>
       <div id="portal-root">
         <PortalNavbar />
       </div>
