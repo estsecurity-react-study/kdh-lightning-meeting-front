a6d047cbebe9128372d386f3b2f7e0422b68d360
김덕현 <oraclian@estsecurity.com>
2023-01-05 16:15:00 +0900

    feat: 폰트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,35 @@
 import { cache } from '@emotion/css';
 import { CacheProvider } from '@emotion/react';
+import { Noto_Sans_KR } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
+import 'twin.macro';
 
 import GlobalStyles from '../styles/GlobalStyles';
 
+const notoSansKr = Noto_Sans_KR({
+  weight: ['400', '500', '700', '900'],
+  subsets: ['korean', 'latin'],
+  variable: '--font-noto-sans-kr',
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <CacheProvider value={cache}>
-      <SWRConfig
-        value={{
-          fetcher: (resource, init) =>
-            fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </SWRConfig>
-    </CacheProvider>
+    <>
+      <CacheProvider value={cache}>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <GlobalStyles />
+          <div className={`${notoSansKr.variable}`} tw="font-sans">
+            <Component {...pageProps} />
+          </div>
+        </SWRConfig>
+      </CacheProvider>
+    </>
   );
 };
 
